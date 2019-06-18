6904791ac924052f20b69cfdacdee54d8efef119
Angelo Giacco <giacco.angelo.s@gmail.com>
2019-06-18 22:23:22 +0100

    test new comments

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ module.exports = app => {
     // `context` extracts information from the event, which can be passed to
     // GitHub API calls. This will return:
     //   { owner: 'yourname', repo: 'yourrepo', number: 123, body: 'Hello World! }
-    const params = context.issue({ body: 'Hello World!' })
+    const tempParams = context.issue()
+    const owner = tempParams.owner
+    const ownerType = context.issue.user.login
+    const params = context.issue({ body: ownerType })
 
     // Post a comment on the issue
     return context.github.issues.createComment(params)
